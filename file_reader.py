2ab75ea809c6b25aca6d5cdc2ec54532b27d00fd
DanMark <daniel.mark.kiss@gmail.com>
2023-10-15 19:15:56 +0200

    feat: :sparkles: image to array conversion

--- file_reader.py
+++ file_reader.py
@@ -1,10 +1,18 @@
 import os
+from PIL import Image
+import numpy as np
 
 class FileReader:
     def __init__(self):
         self.subdirectories = [f.path for f in os.scandir(os.getcwd()) if f.is_dir()]
-         
+
     def show(self):
         # Print the list of subdirectories
         for subdir in self.subdirectories:
             print(subdir)
+    def image_to_array(self, image_path):
+        img = Image.open(image_path)
+        img_array = np.array(img)
+        print(f"Shape of the image array: {img_array.shape}")
+        print(img_array)
+        return img_array
--- program.py
+++ program.py
@@ -4,5 +4,6 @@ def program():
     print("Semmiti")
     reader = FileReader()
     reader.show()
+    reader.image_to_array('/Users/kissdanielmark/Documents/01.Iskola/MSc/2/MediaAndTextmining/character-recognition/Train1/Sample001/img001-00001.png')
 
 program()
