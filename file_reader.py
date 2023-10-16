dcb6259d29d2ae19fc172580c3789d2b794e05fc
DanMark <daniel.mark.kiss@gmail.com>
2023-10-16 15:17:39 +0200

    feat: :sparkles: reading

--- file_reader.py
+++ file_reader.py
@@ -14,9 +14,9 @@ class FileReader:
         # Print the list of subdirectories
         '''for subdir in self.subdirectories:
             print(subdir)
-        '''
+        
         for file in self.files:
-            print(file)
+            #print(file)'''
         print(len(self.files))
     
     def _list_files_recursively(self, directory):
@@ -30,14 +30,18 @@ class FileReader:
     def read_all_files(self):
         for subdir in self.subdirectories:
             #print(subdir)
-            if 'Train2' in subdir or'Train1' in subdir:
+            if ('Train2' in subdir) or ('Train1' in subdir):
                 self._list_files_recursively(subdir)
+
+    def convert_all_to_array(self):
+        for image in self.files:
+            self._image_to_array(image)
 
     def _image_to_array(self, image_path):
         img = Image.open(image_path)
         img_array = np.array(img)
-        print(f"Shape of the image array: {img_array.shape}")
-        print(img_array)
+        #print(f"Shape of the image array: {img_array.shape}")
+        #print(img_array)
         normlaized_img_array  = img_array / 255.0
-        print(normlaized_img_array)
+        #print(normlaized_img_array)
         return img_array
--- program.py
+++ program.py
@@ -1,10 +1,9 @@
 from file_reader import FileReader
 
 def program():
-    print("Semmiti")
     reader = FileReader()
     reader.read_all_files()
+    reader.convert_all_to_array()
     reader.show()
-    #reader.image_to_array('/Users/kissdanielmark/Documents/01.Iskola/MSc/2/MediaAndTextmining/character-recognition/Train1/Sample001/img001-00001.png')
-
+    
 program()
