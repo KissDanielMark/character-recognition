c901b56fc7f4010b3b21b113106230cb16a2602c
DanMark <daniel.mark.kiss@gmail.com>
2023-10-16 15:30:33 +0200

    Update program.py

--- program.py
+++ program.py
@@ -5,5 +5,7 @@ def program():
     reader.read_all_files()
     reader.convert_all_to_array()
     reader.show()
+
+    #reader._image_to_array('/Users/kissdanielmark/Documents/01.Iskola/MSc/2/MediaAndTextmining/character-recognition/Train1/Sample001/img001-00001.png')
     
 program()
