64dfccd3b71d09a2fc9e90d3877ee551c163f171
DanMark <daniel.mark.kiss@gmail.com>
2023-10-23 14:28:15 +0200

    feat: :sparkles: started the labeler

--- labeler.py
+++ labeler.py
@@ -1,3 +1,6 @@
 class Labeler:
     """This class is responsible for giving accurate and reasonable label value for train set"""
-    
+    def __init__(self):
+        self.values = {
+            "img001": "0"
+        }
