bd3e8e924ff10987b24a57e737dd01be14f5f629
DanMark <daniel.mark.kiss@gmail.com>
2023-11-11 14:46:22 +0100

    running both CPU and GPU but different accuracy

--- proba.py
+++ proba.py
@@ -1,14 +1,55 @@
 import tensorflow as tf
+import tensorflow_datasets as tfds
 
-cifar = tf.keras.datasets.cifar100
-(x_train, y_train), (x_test, y_test) = cifar.load_data()
-model = tf.keras.applications.ResNet50(
-    include_top=True,
-    weights=None,
-    input_shape=(32, 32, 3),
-    classes=100,
+BATCH_SIZE = 64
+EPOCHS = 100
+AUTOTUNE = tf.data.AUTOTUNE
+
+# load MNIST
+print("\ndownload mnist")
+(ds_train, ds_test), ds_info = tfds.load(
+    "mnist",
+    split=["train", "test"],
+    shuffle_files=True,
+    as_supervised=True,
+    with_info=True,
 )
 
-loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False)
-model.compile(optimizer="adam", loss=loss_fn, metrics=["accuracy"])
-model.fit(x_train, y_train, epochs=5, batch_size=64)
+
+def normalize_img(image, label):
+    """Normalizes images: `uint8` -> `float32`."""
+    return tf.cast(image, tf.float32) / 255.0, label
+
+
+ds_train = ds_train.map(normalize_img, num_parallel_calls=AUTOTUNE)
+ds_train = ds_train.cache()
+ds_train = ds_train.shuffle(ds_info.splits["train"].num_examples)
+ds_train = ds_train.batch(BATCH_SIZE)
+ds_train = ds_train.prefetch(AUTOTUNE)
+
+ds_test = ds_test.map(normalize_img, num_parallel_calls=AUTOTUNE)
+ds_test = ds_test.cache()
+ds_test = ds_test.batch(BATCH_SIZE)
+
+print("\ncreate and compile model")
+model = tf.keras.Sequential(
+    [
+        tf.keras.layers.Conv2D(16, 3, padding="same", activation="relu"),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Conv2D(32, 3, padding="same", activation="relu"),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Conv2D(64, 3, padding="same", activation="relu"),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(128, activation="relu"),
+        tf.keras.layers.Dense(10),
+    ]
+)
+
+model.compile(
+    optimizer="rmsprop",
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=["accuracy"],
+)
+
+model.fit(ds_train, epochs=EPOCHS, validation_data=ds_test)
